73cc9454a43997eb21d92f2f60bbc7a30cb2b6b0
Shaunak <shaunakdeo66@gmail.com>
2023-12-10 22:07:35 -0500

    ec2 error

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = '8)810zj@#^2xp=1=2rkozbv8#)gub6m1a^9qf&)d-9&x9*c2a_'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Application definition
